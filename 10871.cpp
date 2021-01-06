b34b1d8a41afa4ce17f48166baf23fe35abc284f
unknown <rnjsguswo012@hufs.ac.kr>
2021-01-06 17:04:10 +0900

    for

---
+++ 10871.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n, x;
+	scanf("%d %d", &n, &x);
+
+	for (int i = 0; i < n; i++) {
+		int t;
+		scanf("%d", &t);
+
+		if (t < x) {
+			printf("%d ", t);
+		}
+	}
+
+	return 0;
+}
