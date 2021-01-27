e81be34c49c18f1100608342d93319eb930cb59b
unknown <rnjsguswo012@hufs.ac.kr>
2021-01-27 15:06:56 +0900

    recursion

--- 11729.cpp
+++ 11729.cpp
@@ -1,19 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+int pow(int x, int n) {
+	if (n == 1) {
+		return x;
+	}
+	else {
+		if (n % 2 == 0) {
+			return pow(x * x, n / 2);
+		}
+		else {
+			return x * pow(x * x, (n - 1) / 2);
+		}
+	}
+}
 
 void hanoi(int n, int from, int tmp, int to) {
 	if (n == 1) {
-		cout << 
+		printf("%d %d\n", from, to);
+	}
+	else {
+		hanoi(n - 1, from, to, tmp);
+		printf("%d %d\n", from, to);
+		hanoi(n - 1, tmp, from, to);
 	}
 }
 
 int main() {
 	int n;
-	cin >> n;
+	scanf("%d", &n);
 
-	hanoi(n);
+	printf("%d\n", pow(2, n) - 1);
+	hanoi(n, 1, 2, 3);
 
 	return 0;
 }
